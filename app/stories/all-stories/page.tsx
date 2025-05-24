a288947a8c4adf90286e7d40faf6d436774ff0c7
ochudi <chukwudi.ofoma@pau.edu.ng>
2025-05-24 10:29:50 +0100

    Add stories

--- app/stories/all-stories/page.tsx
+++ app/stories/all-stories/page.tsx
@@ -3,7 +3,7 @@
 import { motion } from "framer-motion";
 import Image from "next/image";
 import Link from "next/link";
-import { stories } from "@/lib/stories"; // <-- ✅ import here
+import { stories } from "@/lib/stories";
 
 const fadeInUp = {
   hidden: { opacity: 0, y: 20 },
