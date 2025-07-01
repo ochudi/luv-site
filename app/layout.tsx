220451e25ce611459503db9d99a6f77e6e594df4
ochudi <chukwudi.ofoma@pau.edu.ng>
2025-07-01 12:27:05 +0100

    Add favicon image

--- app/layout.tsx
+++ app/layout.tsx
@@ -51,6 +51,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+      </head>
       <body className={`${workSans.className} antialiased`}>
         <ThemeProvider
           attribute="class"
